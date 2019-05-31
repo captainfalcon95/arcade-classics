0f9c45e4da1cce800fe6720dc1b8aab359d71bcf
Tristan Falcon <tristan.falcon.dev@gmail.com>
2019-05-31 00:08:59 -0500

    random food generated on game canvas

--- snake/snake.js
+++ snake/snake.js
@@ -3,8 +3,19 @@ const game_canvas = document.getElementById("game_canvas");
 const ctx = game_canvas.getContext("2d");
 ctx.fillStyle = "black";
 ctx.strokeStyle = "white";
-ctx.strokeRect(0, 0, game_canvas.clientWidth, game_canvas.height);
+ctx.strokeRect(0, 0, game_canvas.width, game_canvas.height);
 
 function gameLoop() {
-    console.log('Test Function: Working!');
+    makeFood();
 }
+
+function makeFood() {
+    x_coordinate = Math.floor(Math.random() * ((57-0) + 0) * 10);
+    y_coordinate = Math.floor(Math.random() * ((38-0) + 0) * 10);
+
+    ctx.strokeStyle = "red";
+    ctx.fillStyle = "red";
+    ctx.strokeRect(x_coordinate, y_coordinate, 20, 20);
+    ctx.fillRect(x_coordinate, y_coordinate, 20, 20);
+}
+
